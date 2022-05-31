51a97742f7c3c8a74d9e25caf4c0b289dfbba5ab
Camila Dornas <camiladornas@gmail.com>
2022-05-31 05:13:02 -0300

    Atualização conforme necessidade do swagger

--- project/src/api/routes/index.js
+++ project/src/api/routes/index.js
@@ -4,7 +4,13 @@ const userRouter = require('./userRouter');
 const loginRouter = require('./loginRouter');
 const walletRouter = require('./walletRouter');
 const error = require('../../middlewares/error');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
+
 const app = express();
+
+app.use('/api/v1/docs',
+    swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use('/user', userRouter);
